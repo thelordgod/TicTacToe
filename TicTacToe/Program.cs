d605339c6088fdfdd57323c15662611a1e3db59f
thelordgod <39473749+thelordgod@users.noreply.github.com>
2021-02-16 01:00:39 +0200

    Initial commit - only a working interface so far

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        private static readonly Board _board = new Board();
+        private static Player _currentPlayer = Player.One;
+
+        static void Main(string[] args)
+        {
+            bool isWinner = false;
+            
+            DrawBoard();
+            while (!isWinner)
+            {
+                ReadMove();
+                Console.WriteLine();
+                DrawBoard();
+                _currentPlayer = _currentPlayer == Player.One ? Player.Two : Player.One;
+            }
+        }
+
+        private static void ReadMove()
+        {
+            var input = Console.ReadKey();
+
+            ChangeField(input);
+        }
+
+        private static void ChangeField(ConsoleKeyInfo input)
+        {
+            if (int.TryParse(input.Key.ToString().Remove(0, 1), out int result))
+            {
+                switch (result)
+                {
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8:
+                    case 9:
+                        ProcessField(result);
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Try again.");
+                ReadMove();
+            }
+        }
+
+        private static void ProcessField(int inputKey)
+        {
+            if (_board.squareStatus[key: inputKey] == Status.Empty)
+            {
+                _board.squareStatus[key: inputKey] =
+                    _currentPlayer == Player.One
+                        ? Status.PlayerOne
+                        : Status.PlayerTwo;
+            }
+            else
+            {
+                Console.WriteLine("Field already occupied!");
+                ReadMove();
+            }
+        }
+
+        private static void DrawBoard()
+        {
+            var builder = new StringBuilder();
+
+            for (int i = 1; i <= 3; i++)
+                builder.Append(GetFieldValue(i));
+            builder.AppendLine();
+            for (int i = 4; i <= 6; i++)
+                builder.Append(GetFieldValue(i));
+            builder.AppendLine();
+            for (int i = 7; i <= 9; i++)
+                builder.Append(GetFieldValue(i));
+
+            Console.WriteLine(builder);
+        }
+
+        private static string GetFieldValue(int field)
+        {
+            return _board.squareStatus[key: field] switch
+            {
+                Status.Empty => field.ToString(),
+                Status.PlayerOne => "X",
+                Status.PlayerTwo => "O",
+                _ => null
+            };
+        }
+    }
+
+    internal enum Player
+    {
+        One,
+        Two
+    }
+
+    internal class Board
+    {
+        public Status Status { get; set; }
+        public readonly Dictionary<int, Status> squareStatus = new Dictionary<int, Status>()
+        {
+            { 1, Status.Empty},
+            { 2, Status.Empty},
+            { 3, Status.Empty},
+            { 4, Status.Empty},
+            { 5, Status.Empty},
+            { 6, Status.Empty},
+            { 7, Status.Empty},
+            { 8, Status.Empty},
+            { 9, Status.Empty}
+        };
+    }
+
+    internal enum Status
+    {
+        Empty,
+        PlayerOne,
+        PlayerTwo
+    }
+}
